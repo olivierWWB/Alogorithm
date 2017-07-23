1b45e123f0ec5447da4e4ede8155896b4272d577
Wang Wenbo <wangwenbo_i@didichuxing.com>
2017-07-23 12:20:41 +0800

    TwoSum.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] test = {3,2,4};
+        int target = 6;
+        int[] answer = TwoSum.twoSum(test,target);
+        System.out.print("[");
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i]);
+            if(i+1 != answer.length)
+                System.out.print(",");
+        }
+
+        System.out.print("]");
     }
 }
